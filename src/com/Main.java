0c724282dcdd69ce29414d035543c141fd3afa3c
AlfonsoSanchezSe <1902897@alu.murciaeduca.es>
2025-03-11 17:33:42 +0100

    Modificada primera linea

--- src/com/Main.java
+++ src/com/Main.java
@@ -3,7 +3,7 @@ package com;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hola, esta es la primera linea de mi codigo Java");
+    System.out.println("Hola, esta es la modificacion de primera linea de mi codigo Java");
     System.out.println("Segunda linea de mi codigo Java");
   }
 }
