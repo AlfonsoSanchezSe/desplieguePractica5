7c847e2618ff9587c1e0c0db9d7e241ea80c7335
AlfonsoSanchezSe <1902897@alu.murciaeduca.es>
2025-03-11 17:32:15 +0100

    Añadida segunda linea

--- src/com/Main.java
+++ src/com/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
   public static void main(String[] args) {
     System.out.println("Hola, esta es la primera linea de mi codigo Java");
+    System.out.println("Segunda linea de mi codigo Java");
   }
 }
